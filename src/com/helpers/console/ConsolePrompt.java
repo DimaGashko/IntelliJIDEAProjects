b711fad7732319955753258c621e02b1e1225f3f
DimaGashko <dgashko@mail.ua>
2018-10-21 01:08:07 +0300

    promptComments

--- src/com/helpers/console/ConsolePrompt.java
+++ src/com/helpers/console/ConsolePrompt.java
@@ -2,19 +2,40 @@ package com.helpers.console;
 
 import java.util.Scanner;
 
+/**
+ * Запрашивает у пользователя данные через консольный интерфейс
+ */
 public class ConsolePrompt {
     static private Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Запрашивает у пользователя число типа int
+     *
+     * @param title Текст, что будет показан пользователю в запросе
+     * @return число типа int
+     */
     static public int promptInt(String title) {
         System.out.println(title);
         return scanner.nextInt();
     }
 
+    /**
+     * Запрашивает у пользователя число типа double
+     *
+     * @param title Текст, что будет показан пользователю в запросе
+     * @return число типа double
+     */
     static public double promptDouble(String title) {
         System.out.println(title);
         return scanner.nextDouble();
     }
 
+    /**
+     * Запрашивает у пользователя строку (с пробелмами)
+     *
+     * @param title Текст, что будет показан пользователю в запросе
+     * @return строка введенная пользователем
+     */
     static public String promptLine(String title) {
         System.out.println(title);
         String res = scanner.nextLine();
