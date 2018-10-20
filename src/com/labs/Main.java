0f45b359077388313b796ccfa1f85ffa922c01a2
DimaGashko <dgashko@mail.ua>
2018-10-20 16:17:37 +0300

    lab3

--- src/com/labs/Main.java
+++ src/com/labs/Main.java
@@ -1,7 +1,10 @@
 package com.labs;
 
+import com.labs.lab3.Lab3;
+
 public class Main {
     public static void main(String[] args) {
-
+        var lab3 = new Lab3();
+        lab3.run();
     }
 }
---
+++ src/com/labs/lab3/library/Library.java
@@ -0,0 +1,63 @@
+package com.labs.lab3.library;
+
+import java.util.Arrays;
+
+public class Library {
+    private Book[] books;
+
+    public  Library() {
+
+    }
+
+    public Book[] getBooks() {
+        return Arrays.copyOf(books, books.length);
+    }
+
+    /**
+     * Добавляет переданные книги
+     * @param books Книги, что нужно добавить
+     */
+    public void addBooks(Book[] books) {
+        this.books = books;
+
+        Book[] newBooks = Arrays.copyOf(this.books, this.books.length + books.length);
+        System.arraycopy(books, 0, newBooks, this.books.length, books.length);
+    }
+
+    /**
+     * Взвращает массив книг переданного автора
+     * @param author Автор
+     * @return массив книг переданного автора
+     */
+    public Book[] getBooksByAuthor(String author) {
+        return Arrays.stream(books)
+                .filter(book -> book.getAuthor().equalsIgnoreCase(author))
+                .toArray(Book[]::new);
+    }
+    /*
+
+    * */
+
+    /**
+     * Взвращает массив книг переданного издательства
+     * @param publisher Издательство
+     * @return массив книг переданного издательства
+     */
+    public Book[] getBooksByPublisher(String publisher) {
+        return Arrays.stream(books)
+                .filter(book -> book.getPublisher().equalsIgnoreCase(publisher))
+                .toArray(Book[]::new);
+    }
+
+    /**
+     * Взвращает массив книг изданных после переданного года
+     * @param year Год
+     * @return массив книг изданных после переданного года
+     */
+    public Book[] getBooksAfterYear(int year) {
+        return Arrays.stream(books)
+                .filter(book -> book.getYear() > year)
+                .toArray(Book[]::new);
+    }
+
+}
