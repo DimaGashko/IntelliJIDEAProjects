b6ea9e702e5b82cf338bd28315fb8d6ff997c73a
DimaGashko <dgashko@mail.ua>
2018-10-21 00:56:48 +0300

    promptInprove

--- src/com/helpers/console/ConsolePrompt.java
+++ src/com/helpers/console/ConsolePrompt.java
@@ -5,22 +5,24 @@ import java.util.Scanner;
 public class ConsolePrompt {
     static private Scanner scanner = new Scanner(System.in);
 
-    static public String promptLine(String title) {
-        System.out.println(title);
-        return scanner.nextLine();
-    }
-
     static public int promptInt(String title) {
         System.out.println(title);
-        int res = scanner.nextInt();
-        scanner.nextLine();
-        return res;
+        return scanner.nextInt();
     }
 
     static public double promptDouble(String title) {
         System.out.println(title);
-        double res = scanner.nextDouble();
-        scanner.nextLine();
+        return scanner.nextDouble();
+    }
+
+    static public String promptLine(String title) {
+        System.out.println(title);
+        String res = scanner.nextLine();
+
+        if (res.isEmpty()) {
+            res = scanner.nextLine();
+        }
+
         return res;
     }
 
