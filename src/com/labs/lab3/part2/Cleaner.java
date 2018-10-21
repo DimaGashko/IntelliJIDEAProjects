172691897d0060580968a180af38b62667ab052f
DimaGashko <dgashko@mail.ua>
2018-10-21 03:07:40 +0300

    deleteFromWords

--- src/com/labs/lab3/part2/Cleaner.java
+++ src/com/labs/lab3/part2/Cleaner.java
@@ -3,15 +3,20 @@ package com.labs.lab3.part2;
 import java.util.regex.Pattern;
 
 public class Cleaner {
-    public static Pattern WORD_PATTERN = Pattern.compile("(?i)[a-zа-яёіїє\\d\\-_']+");
+    private static Pattern WORD_PATTERN = Pattern.compile("(?i)[a-zа-яёіїє\\d\\-_']+");
+    private static final String NO_CHARS = "[\\-_']";
+
 
     public String deleteFromWords(String text, String delete, int minLen) {
         return WORD_PATTERN.matcher(text).replaceAll((matchResult) -> {
-            String match = matchResult.group();
+            String word = matchResult.group();
+            String cleanWord = word.replaceAll(NO_CHARS, "");
 
+            if (cleanWord.length() >= minLen) {
+                return word.replaceAll(delete, "");
+            }
 
-
-            return match;
+            return word;
         });
     }
 
