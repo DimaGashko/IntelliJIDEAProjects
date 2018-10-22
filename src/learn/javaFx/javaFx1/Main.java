4fe865beb1224e6fd2239bf0eaa241d5799a61f5
DimaGashko <dgashko@mail.ua>
2018-10-22 19:19:07 +0300

    baseOfAJavaFxApps

--- src/learn/javaFx/javaFx1/Main.java
+++ src/learn/javaFx/javaFx1/Main.java
@@ -1,5 +1,34 @@
 package learn.javaFx.javaFx1;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("My first a native JavaFx Application");
+
+        button = new Button("Button");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+
+        primaryStage.setScene(scene);
+
+        //primaryStage.setWidth(200);
+        //primaryStage.setHeight(180);
+
+        primaryStage.show();
+    }
 }
