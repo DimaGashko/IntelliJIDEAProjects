8647fc2f3024e7007209988562e155125f10617f
DimaGashko <dgashko@mail.ua>
2018-10-26 19:15:55 +0300

    lab1

--- src/learn/javaFx/_19_sceneBuilder/Main.java
+++ src/learn/javaFx/_19_sceneBuilder/Main.java
@@ -3,6 +3,7 @@ package learn.javaFx._19_sceneBuilder;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -13,41 +14,11 @@ public class Main extends Application {
     public void start(Stage window) throws Exception {
         window.setTitle("FXML 2");
 
-        Canvas canv = new Canvas();
-        GraphicsContext ctx = canv.getGraphicsContext2D();
-        canv.setHeight(500);
-        canv.setWidth(500);
-
-        VBox root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        root.getChildren().addAll(canv);
-
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         Scene scene = new Scene(root, 500, 300);
         window.setScene(scene);
 
         window.show();
-
-        draw(ctx);
-
-        new AnimationTimer() {
-            long prev = System.nanoTime();
-
-            public void handle(long currentNanoTime) {
-                clean(ctx);
-                draw(ctx);
-                prev = currentNanoTime;
-            }
-        }.start();
-    }
-
-    private double x = 10;
-    private double y = 10;
-
-    public void draw(GraphicsContext ctx) {
-        ctx.fillRect(x++, y++, 10, 10);
-    }
-
-    public void clean(GraphicsContext ctx) {
-        ctx.clearRect(0,0, ctx.getCanvas().getWidth(), ctx.getCanvas().getHeight());
     }
 
     public static void main(String[] args) {
