0e8c308ff51e75f42a951824815acf807a22f54c
DimaGashko <dgashko@mail.ua>
2018-11-01 20:41:19 +0200

    edit

--- src/learn/javaFx/_17_FXML/Controller.java
+++ src/learn/javaFx/_17_FXML/Controller.java
@@ -1,13 +1,14 @@
 package learn.javaFx._17_FXML;
 
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
 
     public Button touch;
 
-    public void onClick() {
-        System.out.println("Run some code the user doesn't see");
+    public void onMouseTouch(MouseEvent event) {
+        System.out.println("Run some code the user doesn't see;" + event);
         touch.setText("Stop touching me!");
     }
 }
