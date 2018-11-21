88aa9c3da83770791d33b9bc57391d5dce0c4f94
DimaGashko <dgashko@mail.ua>
2018-11-21 22:39:16 +0200

    edit

--- src/other/computerSience3/lab4/Lab4.java
+++ src/other/computerSience3/lab4/Lab4.java
@@ -1,6 +1,10 @@
 package other.computerSience3.lab4;
 
+import java.nio.charset.Charset;
+
 public class Lab4 {
+    static Charset toCharset = Charset.forName("cp866");
+
     public static void main(String[] args) {
 
     }
