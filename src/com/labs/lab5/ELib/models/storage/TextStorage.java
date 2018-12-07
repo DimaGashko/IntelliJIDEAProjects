923cb7478d3ebf8a2f2810e07f32b91545cdda7b
DimaGashko <dgashko@mail.ua>
2018-12-07 13:11:13 +0200

    commentsInIStorage

--- src/com/labs/lab5/ELib/models/storage/TextStorage.java
+++ src/com/labs/lab5/ELib/models/storage/TextStorage.java
@@ -1,5 +1,29 @@
 package com.labs.lab5.ELib.models.storage;
 
-public class TextStorage implements Storage {
+public class TextStorage<T> implements IStorage {
 
+    @Override
+    public void load() {
+
+    }
+
+    @Override
+    public boolean add(Object item) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Object[] item) {
+        return false;
+    }
+
+    @Override
+    public Object[] getArrOfData() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] getArrOfData(IFilter filter) {
+        return new Object[0];
+    }
 }
