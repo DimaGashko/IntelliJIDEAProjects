a067920b08541090be878fafe6523c5a1b51140a
DimaGashko <dgashko@mail.ua>
2018-12-07 16:16:08 +0200

    TOODS

--- src/com/labs/lab5/ELib/models/storage/TextStorage.java
+++ src/com/labs/lab5/ELib/models/storage/TextStorage.java
@@ -5,20 +5,27 @@ import com.labs.lab3.part1.library.Book;
  * Класс для хранения данных на основании текстового файла
  * @param <T> класс хранимых данных
  * @version 0.0.0.1
+ *
+ * TODO: заменить Array на ArrayList
  */
 public class TextStorage<T> implements IStorage {
     // Массив хранимых данных
     private T[] data;
-    
+
     // Количество данных
     private int len = 0;
 
+    /**
+     * TODO: после внедрения ArrayList удалить либо сделать публичным
+     */
     private TextStorage() {
 
     }
 
     /**
      * @param dataArray массив для хранения данных
+     *
+     * TODO: после внедрения ArrayList удалить этот конструктор
      */
     public TextStorage(T[] dataArray) {
         data = dataArray;
