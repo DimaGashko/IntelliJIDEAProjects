225ce8d898f88a632aa76e89439377b48ed1ff45
DimaGashko <dgashko@mail.ua>
2018-12-09 20:56:59 +0200

    getSetFilters

--- src/com/labs/lab5/ELib/models/BookFilters.java
+++ src/com/labs/lab5/ELib/models/BookFilters.java
@@ -20,5 +20,75 @@ public class BookFilters {
 
     }
 
-    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public double getPriceFrom() {
+        return priceFrom;
+    }
+
+    public void setPriceFrom(double priceFrom) {
+        this.priceFrom = priceFrom;
+    }
+
+    public double getPriceTo() {
+        return priceTo;
+    }
+
+    public void setPriceTo(double priceTo) {
+        this.priceTo = priceTo;
+    }
+
+    public int getPagesFrom() {
+        return pagesFrom;
+    }
+
+    public void setPagesFrom(int pagesFrom) {
+        this.pagesFrom = pagesFrom;
+    }
+
+    public int getPagesTo() {
+        return pagesTo;
+    }
+
+    public void setPagesTo(int pagesTo) {
+        this.pagesTo = pagesTo;
+    }
+
+    public LocalDate getDateFrom() {
+        return dateFrom;
+    }
+
+    public void setDateFrom(LocalDate dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+
+    public LocalDate getDateTo() {
+        return dateTo;
+    }
+
+    public void setDateTo(LocalDate dateTo) {
+        this.dateTo = dateTo;
+    }
 }
