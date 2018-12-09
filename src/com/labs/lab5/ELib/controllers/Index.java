9b23346ba3deddcf3adcdd082c5512db053872da
DimaGashko <dgashko@mail.ua>
2018-12-09 18:42:49 +0200

    BaseConstructionr

--- src/com/labs/lab5/ELib/controllers/Index.java
+++ src/com/labs/lab5/ELib/controllers/Index.java
@@ -3,11 +3,14 @@ package com.labs.lab5.ELib.controllers;
 import com.labs.lab3.part1.library.Book;
 import com.labs.lab5.ELib.models.storage.IStorage;
 import com.labs.lab5.ELib.models.storage.TextStorage;
+
+import com.jfoenix.controls.*;
+import javafx.scene.control.*;
+import javafx.fxml.FXML;
+
 import javafx.fxml.Initializable;
 
-import java.lang.reflect.Array;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class Index implements Initializable {
@@ -16,13 +19,32 @@ public class Index implements Initializable {
     // Объект для хранения книг
     private IStorage<Book> storage;
 
+    @FXML private MenuItem fxMenuAddBook;
+    @FXML private MenuItem fxMenuResetFilters;
+    @FXML private MenuItem fxMenuAppExit;
+    @FXML private MenuItem fxMenuEditBook;
+    @FXML private MenuItem fxMenuRemoveBook;
+    @FXML private MenuItem fxMenuHelp;
+    @FXML private MenuItem fxMenuAbout;
+    @FXML private JFXButton fxToolRemove;
+    @FXML private JFXButton fxToolEdit;
+    @FXML private JFXButton fxToolAdd;
+    @FXML private JFXTextField fxFilterName;
+    @FXML private JFXTextField fxFilterAuthor;
+    @FXML private JFXTextField fxFilterPublisher;
+    @FXML private JFXSlider fxFilterPriceFrom;
+    @FXML private JFXSlider fxFilterPriceTo;
+    @FXML private Label fxFilterPagesFrom;
+    @FXML private JFXSlider fxFilterPagesTo;
+    @FXML private JFXDatePicker fxFilterDateFrom;
+    @FXML private JFXDatePicker fxFilterDateTo;
+    @FXML private JFXButton fxResetFilters;
+    @FXML private JFXTreeTableView<?> fxBooksTable;
+    @FXML private Label fxNoBooks;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initStorage();
-
-        storage.add(new Book());
-
-        System.out.println(Arrays.toString(storage.getArrOfData()));
     }
 
     private void initStorage() {
