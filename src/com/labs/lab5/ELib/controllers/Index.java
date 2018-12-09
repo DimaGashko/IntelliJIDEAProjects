1f4428fce242fdab5aed1fdb8d53023fd72599d6
DimaGashko <dgashko@mail.ua>
2018-12-09 18:59:59 +0200

    initEventMethods

--- src/com/labs/lab5/ELib/controllers/Index.java
+++ src/com/labs/lab5/ELib/controllers/Index.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXML;
 
 import javafx.fxml.Initializable;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,6 +22,7 @@ public class Index implements Initializable {
 
     @FXML private MenuItem fxMenuAddBook;
     @FXML private MenuItem fxMenuResetFilters;
+    @FXML private MenuItem fxMenuRemoveBooks;
     @FXML private MenuItem fxMenuAppExit;
     @FXML private MenuItem fxMenuEditBook;
     @FXML private MenuItem fxMenuRemoveBook;
@@ -46,6 +48,87 @@ public class Index implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         initStorage();
     }
+
+    @FXML
+    void fxOnActionFilterAuthor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionFilterDateFrom(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionFilterDateTo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionFilterName(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionFilterPublisher(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionMenuAbout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionMenuAddBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionMenuAppExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionMenuEditBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionMenuHelp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionMenuRemoveBooks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionMenuResetFilters(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionResetFilters(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionToolAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionToolEdit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fxOnActionToolRemove(ActionEvent event) {
+
+    }
+
 
     private void initStorage() {
         storage = new TextStorage<>(DB_URL, Book::toString, Book::parse, Book.class);
