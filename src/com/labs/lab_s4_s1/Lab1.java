c730a6c1386c0e0d4b0edbc78071696be8cb9d8c
DimaGashko <dgashko@mail.ua>
2019-02-17 16:52:27 +0200

    user

---
+++ src/com/labs/lab_s4_s1/Lab1.java
@@ -0,0 +1,9 @@
+package com.labs.lab_s4_s1;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        User user = new User("Katya", "Klep");
+
+        System.out.println(user);
+    }
+}
--- src/com/labs/lab_s4_s1/User.java
+++ src/com/labs/lab_s4_s1/User.java
@@ -1,6 +1,7 @@
 package com.labs.lab_s4_s1;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.UUID;
 
 public class User {
@@ -12,4 +13,88 @@ public class User {
     private LocalDate registered;
     private int age;
 
+    private User() {
+
+    }
+
+    public User(String firstName, String lastName) {
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public String toString() {
+        return getFullName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return online == user.online &&
+                age == user.age &&
+                Objects.equals(id, user.id) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(country, user.country) &&
+                Objects.equals(registered, user.registered);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, country, online, registered, age);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean isOnline() {
+        return online;
+    }
+
+    public void setOnline(boolean online) {
+        this.online = online;
+    }
+
+    public LocalDate getRegistered() {
+        return registered;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
