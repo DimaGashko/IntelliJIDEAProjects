825721d96f59287079d5b9e0fbab871f39e0924b
DimaGashko <dgashko@mail.ua>
2019-02-17 18:54:43 +0200

    baseSaveLoad

--- src/com/labs/lab_s4_s1/Lab1.java
+++ src/com/labs/lab_s4_s1/Lab1.java
@@ -1,11 +1,43 @@
 package com.labs.lab_s4_s1;
 
-import java.util.TreeSet;
+import java.io.*;
+import java.util.HashSet;
 
 public class Lab1 {
-    public static void main(String[] args) {
-        TreeSet<User> users = new TreeSet<>();
+    private static final String USERS_STORAGE_URL = "./users.storage";
 
-        System.out.println(users);
+    private HashSet<User> users = new HashSet<>();
+
+    public static void main(String[] args) {
+        var lab = new Lab1();
+        lab.run();
     }
+
+    public void run() {
+
+    }
+
+    /**
+     * Загружает данные users из файла
+     * <b>users при этом перезаписывается данными из файла</b>
+     */
+    private void loadUsers() throws IOException, ClassNotFoundException {
+        try(FileInputStream fis = new FileInputStream(USERS_STORAGE_URL);
+            ObjectInputStream ois = new ObjectInputStream(fis)) {
+
+            users = (HashSet<User>)ois.readObject();
+        }
+    }
+
+    /**
+     * Записывает коллекцию users в файл
+     */
+    private void saveUsers() throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(USERS_STORAGE_URL);
+             ObjectOutputStream out = new ObjectOutputStream(fos)) {
+
+            out.writeObject(users);
+        }
+    }
+
 }
