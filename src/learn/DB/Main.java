5929be5b4fd421a777a250219ac27c015a9d7267
DimaGashko <dgashko@mail.ua>
2019-03-16 23:42:43 +0200

    init

--- src/learn/DB/Main.java
+++ src/learn/DB/Main.java
@@ -1,5 +1,23 @@
 package learn.DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
 
+    public static void main(String[] args) {
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/test_db",
+                    "root", "");
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
