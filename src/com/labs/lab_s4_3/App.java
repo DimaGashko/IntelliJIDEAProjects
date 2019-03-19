ef683d3b83e46a2ac670270d9235e0fae01f2e9e
DimaGashko <dgashko@mail.ua>
2019-03-19 22:33:16 +0200

    initApp

---
+++ src/com/labs/lab_s4_3/App.java
@@ -0,0 +1,16 @@
+package com.labs.lab_s4_3;
+
+public class App {
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+
+    public App() {
+
+    }
+
+    public void run() {
+
+    }
+}
