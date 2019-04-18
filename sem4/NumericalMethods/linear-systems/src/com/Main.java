ca0c7766c151a6423d9e291fe80f67c7a25f77e2
DimaGashko <dimagashko@gmail.com>
2019-04-18 11:13:26 +0300

    initSolvingLinearSystem

---
+++ sem4/NumericalMethods/linear-systems/src/com/Main.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
+    private void run() {
+        System.out.println("Hello");
+    }
+
+}
