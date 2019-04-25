e267efc7b2c7b93ceb897e573dd13b6642beea74
DimaGashko <dimagashko@gmail.com>
2019-04-25 03:10:22 +0300

    initInterface

--- sem4/NumericalMethods/linear-systems/src/com/Main.java
+++ sem4/NumericalMethods/linear-systems/src/com/Main.java
@@ -17,26 +17,31 @@ public class Main {
     }
 
     private void run() {
-        int size = promptInt("Enter the number of equations:");
-        Matrix A = askForA();
-        Vector B = askForB();
+        init();
+        runCalc();
+    }
 
+    private void runCalc() {
 
     }
 
-    private Matrix askForA() {
-        int n = promptInt("Enter the number of equations: ");
-        System.out.println("Enter elements of the matrix: ");
+    private void init() {
+        int size = promptInt("Enter the number of equations:");
+        Matrix A = askForA();
+        Vector B = askForB();
+    }
 
-        Matrix matrix = new Matrix(n, n);
+    private Matrix askForA() {
+        System.out.println("Enter elements of the A matrix: ");
+
+        Matrix matrix = new Matrix(size, size);
         matrix.enter();
 
         return matrix;
     }
 
     private Vector askForB() {
-        int size = promptInt("Enter vector size: ");
-        System.out.println("Enter elements of a the vector: ");
+        System.out.println("Enter elements of a B matrix: ");
 
         Vector vector = new Vector(size);
         vector.enter();
