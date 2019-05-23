51e949342fc8c7131e70c79dc0d420ff7b98d950
DimaGashko <dimagashko@gmail.com>
2019-05-23 11:46:36 +0300

    baseLogin

--- sem4/java/memory.pro/src/com/components/login/LoginComponent.java
+++ sem4/java/memory.pro/src/com/components/login/LoginComponent.java
@@ -1,5 +1,10 @@
 package com.components.login;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import lib.Alerts.Alerts;
+import lib.Auth.AuthException;
 import lib.Component.Component;
 
 import java.net.URL;
@@ -7,9 +12,34 @@ import java.util.ResourceBundle;
 
 public class LoginComponent extends Component {
 
+    @FXML
+    private JFXTextField fxUsername;
+
+    @FXML
+    private JFXPasswordField fxPassword;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    private void login() {
+        String username = fxUsername.getText();
+        String password = fxPassword.getText();
+
+        try {
+            global.getAuth().login(username, password);
+        } catch (AuthException e) {
+            alerts.show(Alerts.alertWarn, "Wrong username of password");
+            return;
+        }
+
+        global.setScreen("index");
+    }
+
+    @FXML
+    void onLogin() {
+        login();
     }
 
 }
--- sem4/java/memory.pro/src/com/screens/index/IndexScreen.java
+++ sem4/java/memory.pro/src/com/screens/index/IndexScreen.java
@@ -3,6 +3,7 @@ package com.screens.index;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import lib.Screen.Screen;
+import schemas.User;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,7 +16,17 @@ public class IndexScreen extends Screen implements Initializable {
 
     }
 
-    public void click(ActionEvent actionEvent) {
+    @Override
+    public void showed() {
+        String username = global.getAuth().getLoggedInUsername();
+
+        User user = global.getUserDao().getUserByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Cannot load user data"));
+
+        System.out.println(user);
+    }
+
+    public void click() {
         global.setScreen("auth");
     }
 }
