2fa3f54c88c08bdd0ccb60c626fa37cef5d6b300
DimaGashko <dimagashko@gmail.com>
2019-05-25 23:37:59 +0300

    initProperties

--- sem4/java/memory.pro/src/com/components/MemorizeComponent/MemorizeComponent.java
+++ sem4/java/memory.pro/src/com/components/MemorizeComponent/MemorizeComponent.java
@@ -1,10 +1,15 @@
 package com.components.MemorizeComponent;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import lib.Component.Component;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MemorizeComponent extends Component {
+
+    private SimpleIntegerProperty dataCount = new SimpleIntegerProperty(0);
+    private SimpleIntegerProperty curDataIndex = new SimpleIntegerProperty(0);
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -12,7 +17,30 @@ public class MemorizeComponent extends Component {
     }
 
     public void run(String trainingType, int dataCount) {
-
+        this.dataCount.set(dataCount);
     }
 
+    public int getDataCount() {
+        return dataCount.get();
+    }
+
+    public SimpleIntegerProperty dataCountProperty() {
+        return dataCount;
+    }
+
+    public void setDataCount(int dataCount) {
+        this.dataCount.set(dataCount);
+    }
+
+    public int getCurDataIndex() {
+        return curDataIndex.get();
+    }
+
+    public SimpleIntegerProperty curDataIndexProperty() {
+        return curDataIndex;
+    }
+
+    public void setCurDataIndex(int curDataIndex) {
+        this.curDataIndex.set(curDataIndex);
+    }
 }
