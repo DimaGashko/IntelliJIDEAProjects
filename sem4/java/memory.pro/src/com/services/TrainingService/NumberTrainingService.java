2fb03e7a1beff3f5c8f9b283d4b7fcc8725270df
DimaGashko <dimagashko@gmail.com>
2019-05-26 17:37:58 +0300

    minGrade

--- sem4/java/memory.pro/src/com/services/TrainingService/NumberTrainingService.java
+++ sem4/java/memory.pro/src/com/services/TrainingService/NumberTrainingService.java
@@ -45,6 +45,10 @@ public class NumberTrainingService extends TrainingService {
         result.setDateTime(startTime);
         result.setUser(user);
 
+        if (result.getGrade() <= 0) {
+            return -1;
+        }
+
         resultDao.add(result);
         resultDataDao.addAll(resultData);
 
