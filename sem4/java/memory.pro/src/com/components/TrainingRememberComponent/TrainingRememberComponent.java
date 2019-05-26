d40334fdf021f4076bf04e8654fd4b7bd4505f17
DimaGashko <dimagashko@gmail.com>
2019-05-26 10:56:08 +0300

    initRememberComponent

---
+++ sem4/java/memory.pro/src/com/components/TrainingRememberComponent/TrainingRememberComponent.java
@@ -0,0 +1,49 @@
+package com.components.TrainingRememberComponent;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import lib.Alerts.Alerts;
+import lib.Component.Component;
+import lib.Validation.Validation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TrainingRememberComponent extends Component {
+
+    private OnDoneCallback onDoneCallback;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initValidation();
+    }
+
+    public void run(OnDoneCallback onDoneCallback) {
+        this.onDoneCallback = onDoneCallback;
+    }
+
+    private void done() {
+        if (!isValid()) return;
+
+        onDoneCallback.call();
+    }
+
+    private void initValidation() {
+
+    }
+
+    private boolean isValid() {
+        return false;
+    }
+
+    @FXML public void onDone() {
+        done();
+    }
+
+    @FunctionalInterface
+    public interface OnDoneCallback {
+        void call();
+    }
+}
